f04a66edf65c771ed14189d63910b67bd5d945af
Stefan Kropp <stefan@debxwoody.de>
2018-12-07 19:52:16 +0100

    Configuration example IMAP

---
+++ config/generate-imap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FILE=neomuttrc-imap-example
+
+SEP="##########################################################################"
+
+echo $SEP >> $FILE
+echo "#  IMAP - Neomutt configuration" >> $FILE
+echo $SEP >> $FILE
+
+cat variable/01-002-realname.txt >> $FILE
+cat variable/01-003-from.txt >> $FILE
+cat variable/02-003-spoolfile.txt >> $FILE
+
+echo "# set spoolfile=\"imaps://domain.tld/\"" >> $FILE
+
+cat variable/02-007-mailboxes.txt >> $FILE
+
+echo "mailboxes \$spoolfile" >> $FILE
+
+cat variable/07-001-imap_user >> $FILE
+cat variable/07-002-imap_pass >> $FILE
+
